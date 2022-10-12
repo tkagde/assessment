b5f84380f49d4b04a2be7a1183e94e6ecf747f75
tkagde <114738266+tkagde@users.noreply.github.com>
2022-10-12 23:31:41 +0100

    question 1

--- src/main/java/com/blueyonder/assessment/domain/OrderShipmentInput.java
+++ src/main/java/com/blueyonder/assessment/domain/OrderShipmentInput.java
@@ -3,26 +3,19 @@
  */
 package com.blueyonder.assessment.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 /**
  * @author tkagde
  *
  */
+@NoArgsConstructor
+@Getter
+@Setter
+@AllArgsConstructor
 public class OrderShipmentInput {
-	
 	private String orderId;
-
-	public String getOrderId() {
-		return orderId;
-	}
-
-	public void setOrderId(String orderId) {
-		this.orderId = orderId;
-	}
-
-	public OrderShipmentInput(String orderId) {
-		super();
-		this.orderId = orderId;
-	}
-	
-
 }
